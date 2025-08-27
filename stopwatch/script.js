5127c0804c3b9527aae8233df4153b625464d336
user.email <mrdilmurat777@gmail.com>
2025-08-27 13:14:03 +0600

    feat: finished mini project stopwatch

---
+++ stopwatch/script.js
@@ -0,0 +1,39 @@
+let time = document.getElementById('display-time');
+
+
+let [second, minute, hour] = [0, 0, 0];
+let timer = null
+
+function stopWatch() {
+	second++;
+	if (second == 60) {
+		second = 0;
+		minute++;
+		if (minute == 60) {
+			minute = 0;
+			hour++;
+		}
+	}
+
+	let h = hour < 10 ? "0" + hour : hour;
+	let m = minute < 10 ? "0" + minute : minute;
+	let s = second < 10 ? "0" + second : second;
+
+	time.innerHTML = h + ":" + m + ":" + s
+}
+
+function startWatch() {
+	if (timer !== null) {
+		clearInterval(timer)
+	}
+	timer = setInterval(stopWatch, 1000)
+}
+
+function watchStop() {
+	clearInterval(timer);
+}
+function watchReset() {
+	clearInterval(timer);
+	[second, minute, hour] = [0, 0, 0];
+	time.innerHTML = "00:00:00";
+}
